d932e2448351ec7dd78ac968c6893622bacd5ecd
chenzhiliang94@gmail.com <chenzhiliang94@gmail.com>
2023-03-29 19:19:35 +0800

    add requirements.txt

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import pandas as pd
 
 # Import data
 import scipy.io
-
-from matplotlib import pyplot
-import matplotlib.pyplot as plt
-plt.figure(figsize=(8, 6), dpi=80)
-plt.rcParams.update({'figure.max_open_warning': 0})
 import time
 
 # Import helper functions
@@ -19,8 +14,7 @@ from lib.utils import construct_knn_graph
 from lib.louvain_priors import Louvain
 from lib.utils import read_priors
 
-# Louvain algorithm
-import community
+from community import community_louvain # for checking of accuracy of louvain
 import networkx as nx
 
 # Remove warnings
@@ -28,6 +22,19 @@ import warnings
 warnings.filterwarnings("ignore")
 import copy
 import math
+
+def get_clusters(custom_partition):
+    nc_louvain = len(np.unique( [custom_partition[nodes] for nodes in custom_partition.keys()] ))
+    n = len(Wnx.nodes())
+    C = np.zeros([n])
+    clusters = []
+    k = 0
+    for com in set(custom_partition.values()):
+        list_nodes = [nodes for nodes in custom_partition.keys() if custom_partition[nodes] == com]
+        C[list_nodes] = k
+        k += 1
+        clusters.append(list_nodes)
+    return C
 
 if __name__ == '__main__':
     
@@ -49,6 +56,7 @@ if __name__ == '__main__':
     scaler = MinMaxScaler()
     data_scaled = pd.DataFrame(scaler.fit_transform(numerical_data), columns=numerical_data.columns)
 
+    # get ground truth from pokemon data
     def get_ground_truth_partial_cluster(louvain_communities):
         # returns a list of numerical cluster e.g [0,0,0,1,1,1...] of a smaller list size (since only partial labels are available)
         ground_truth_communities = []
@@ -65,45 +73,31 @@ if __name__ == '__main__':
     W = construct_knn_graph(data_scaled.to_numpy(),10,dist='euclidean')
     Wnx = nx.from_numpy_array(W)
 
+    # custom louvain with priors
     custom_Louvain = Louvain()
     coupling_priors, decoupling_priors = read_priors(W.shape[0])
-    custom_partition = custom_Louvain.getBestPartition(Wnx, 1.5, coupling_priors, decoupling_priors, 0)
-    nc_louvain = len(np.unique( [custom_partition[nodes] for nodes in custom_partition.keys()] ))
-    n = len(Wnx.nodes())
-    print("")
+    custom_partition = custom_Louvain.getBestPartition(Wnx, 1, coupling_priors, decoupling_priors, 0)
+    nc_louvain_prior = len(np.unique( [custom_partition[nodes] for nodes in custom_partition.keys()] ))
 
-    # Extract clusters
-    Clouv = np.zeros([n])
-    clusters = []
-    k = 0
-    for com in set(custom_partition.values()):
-        list_nodes = [nodes for nodes in custom_partition.keys() if custom_partition[nodes] == com]
-        Clouv[list_nodes] = k
-        k += 1
-        clusters.append(list_nodes)
-
-    GTClusters,partial_Clouv_no_priors = get_ground_truth_partial_cluster(Clouv)
+    GTClusters,partial_Clouv_no_priors = get_ground_truth_partial_cluster(get_clusters(custom_partition))
 
     # Baseline accuracy
     Crand = np.random.randint(0,8,[len(GTClusters)])
     acc = compute_purity(Crand,GTClusters,8)
- 
-    custom_Louvain = Louvain()
-    custom_partition = custom_Louvain.getBestPartition(Wnx, 1.5, coupling_priors, decoupling_priors, 50)
-    nc_louvain = len(np.unique( [custom_partition[nodes] for nodes in custom_partition.keys()] ))
-    n = len(Wnx.nodes())
     
-
-    # Extract clusters
-    Clouv = np.zeros([n])
-    clusters = []
-    k = 0
-    for com in set(custom_partition.values()):
-        list_nodes = [nodes for nodes in custom_partition.keys() if custom_partition[nodes] == com]
-        Clouv[list_nodes] = k
-        k += 1
-        clusters.append(list_nodes)
-    GTClusters,partial_Clouv = get_ground_truth_partial_cluster(Clouv)
+    # Louvain partition with existing library to check for our implementation accuracy
+    # partition = community_louvain.best_partition(Wnx)
+    # nc_louvain_lib = len(np.unique( [partition[nodes] for nodes in partition.keys()] ))
+    # GTClusters,partial_Clouv_lib = get_ground_truth_partial_cluster(get_clusters(partition))
+ 
+    # custom louvain with no priors
+    custom_Louvain = Louvain()
+    custom_partition = custom_Louvain.getBestPartition(Wnx, 1, coupling_priors, decoupling_priors, 50)
+    nc_louvain_no_prior = len(np.unique( [custom_partition[nodes] for nodes in custom_partition.keys()] ))
+    GTClusters,partial_Clouv = get_ground_truth_partial_cluster(get_clusters(custom_partition))
+    
+    print("\n")
     print("baseline purity score: ", acc)
-    print("louvain with no-priors purity score: ", compute_purity(partial_Clouv_no_priors, GTClusters, nc_louvain))
-    print("louvain with priors purity score: ", compute_purity(partial_Clouv, GTClusters, nc_louvain))
+    #print("louvain external library purity score: ", compute_purity(partial_Clouv_lib, GTClusters, nc_louvain_lib))
+    print("louvain own code with no-priors purity score : ", compute_purity(partial_Clouv_no_priors, GTClusters, nc_louvain_no_prior))
+    print("louvain with priors purity score: ", compute_purity(partial_Clouv, GTClusters, nc_louvain_prior))
